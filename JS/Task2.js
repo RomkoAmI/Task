b3059f81003090ae8264f6e01560ea419770f746
unknown <parejkoroma@gmail.com>
2023-05-15 00:48:47 +0300

    Finish with js

--- JS/Task2.js
+++ JS/Task2.js
@@ -18,3 +18,33 @@ let header2 = document.createElement('h1');
 header2.innerHTML = output2;
 document.body.insertBefore(header2, document.body.firstChild);
 document.body.insertBefore(header1, document.body.firstChild);
+
+
+
+
+
+
+let overlay = document.querySelector('.overlay');
+let largeImage = document.getElementById('bigImage');
+let closeButton = document.querySelector('.closeButton');
+let backgroundOverlay = document.querySelector('.shadow');
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener('click', function() {
+    let imageSrc = this.src;
+    showImage(imageSrc);
+  });
+}
+
+function showImage(imageSrc) {
+    largeImage.src = imageSrc; 
+    overlay.classList.add('active');
+    backgroundOverlay.style.display = 'block';
+}
+
+function closeImage() {
+    overlay.classList.remove('active'); 
+    backgroundOverlay.style.display = 'none';
+    largeImage.src = ''; 
+}
+closeButton.addEventListener('click', closeImage);
